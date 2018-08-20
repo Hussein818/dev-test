60fb0b9422bb89bf2851a297c34cf3a8ba25b866
hussein qudsi <husseinqudsi@husseins-MBP.zyxel.com>
2018-08-19 20:25:52 -0400

    adding update

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var app=http.createServer(function (req, res) {
   //sets the right header and status code
   res.end('Hello World\n');
   //outputs string with line end symbol
-}).listen(1337, "127.0.0.1");
+});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+// }).listen(1337, "127.0.0.1");
 //sets port and IP address of the server
 console.log('Server running at http://127.0.0.1:1337/');
